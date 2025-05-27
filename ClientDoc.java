6f76fa230d949002acea3b8a214e0058ae188c16
Tu Quy <115814143+Tuquyyyy@users.noreply.github.com>
2025-05-27 21:53:20 +0700

    Delete ClientDoc.java

--- ClientDoc.java
+++
@@ -1,8 +0,0 @@
-public class ClientDoc {
-    public Document newDocument(Document builder) {
-        builder.setExtension();
-        builder.setEncrytion();
-        builder.buildDoc();
-        return builder;
-    }
-}
