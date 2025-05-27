94880e3a3c8a828536f4dd1c63a9f4367ae63975
Tu Quy <hieunguyen119955@gmail.com>
2025-05-27 21:51:08 +0700

    Lab 5
    CSE 305

--- PropertyOwner.java
+++ PropertyOwner.java
@@ -1,42 +1,27 @@
-import java.util.List;
 import java.util.ArrayList;
 
 public class PropertyOwner {
-
     private User user;
-    private List<Property> listOfProperties;
+    private ArrayList<Property> properties;
 
     public PropertyOwner(User user) {
         this.user = user;
-        this.listOfProperties = new ArrayList<>();
+        this.properties = new ArrayList<>();
+    }
+
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+
+    public void removeProperty(Property property) {
+        properties.remove(property);
+    }
+
+    public ArrayList<Property> getProperties() {
+        return properties;
     }
 
     public User getUser() {
         return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public List<Property> getListOfProperties() {
-        return listOfProperties;
-    }
-
-    public void addProperty(Property property) {
-        property.setPropertyOwnerID(user.getUserID());
-        listOfProperties.add(property);
-    }
-
-    public void removeProperty(Property property) {
-        listOfProperties.remove(property);
-    }
-
-    @Override
-    public String toString() {
-        return "PropertyOwner{"
-                + "user=" + user
-                + ", listOfProperties=" + listOfProperties
-                + '}';
     }
 }
--- User.java
+++ User.java
@@ -1,12 +1,10 @@
-
 public class User {
-
     private String userID;
     private String password;
     private String email;
     private String userType;
 
-    public void createUser(String userID, String password, String email, String userType) {
+    public User(String userID, String password, String email, String userType) {
         this.userID = userID;
         this.password = password;
         this.email = email;
@@ -15,37 +13,5 @@ public class User {
 
     public void resetPassword(String newPassword) {
         this.password = newPassword;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUserType() {
-        return userType;
-    }
-
-    public void setUserType(String userType) {
-        this.userType = userType;
     }
 }
